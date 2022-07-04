470fc955c0d25e0eee43a5372df142d842bced0a
Rob Thijssen <rthijssen@gmail.com>
2022-07-04 18:13:02 +0300

    update calamari-pc binary

--- daemon/client-update.sh
+++ daemon/client-update.sh
@@ -34,4 +34,25 @@ if dpkg -l manta; then
   if [ ${manta_service_pre_update_state} = active ] && ! systemctl is-active --quiet manta.service; then
     sudo systemctl start manta.service && sleep 60
   fi
+elif systemctl is-active --quiet calamari-pc.service; then
+  if sudo systemctl stop calamari-pc.service; then
+    if [ -f /var/log/calamari-pc/stderr.log ]; then
+      sudo mv /var/log/calamari-pc/stderr.log /var/log/calamari-pc/stderr-$(date --utc --iso-8601=seconds).log
+    fi
+    latest_manta_release_index_url=https://api.github.com/repos/Manta-Network/Manta/releases/latest
+    latest_manta_release_download_url=$(curl \
+      -sL \
+      -H 'Cache-Control: no-cache, no-store' \
+      -H 'Accept: application/vnd.github+json' \
+      ${latest_manta_release_index_url} \
+      | jq -r '.assets[] | select(.name == "manta") | .browser_download_url')
+    if sudo curl \
+      -sLo /usr/local/bin/manta \
+      ${latest_manta_release_download_url} \
+      && chmod +x /usr/local/bin/manta; then
+      sudo rm -f /usr/local/bin/calamari-pc
+      sudo ln -sfr /usr/local/bin/manta /usr/local/bin/calamari-pc
+    fi
+    sudo systemctl start calamari-pc.service && sleep 60
+  fi
 fi
