487a66f1e80d7c46c7bd71f9d8ef9457921c9cc2
Rob Thijssen <rthijssen@gmail.com>
2022-10-04 14:58:11 +0300

    sync-node-state auth-keys

--- daemon/sync-node-state.sh
+++ daemon/sync-node-state.sh
@@ -23,6 +23,12 @@ curl -sL \
 rubberneck_github_latest_sha=$(jq -r .[0].sha ${tmp}/${rubberneck_github_org}-${rubberneck_github_repo}-commits.json)
 rubberneck_github_latest_date=$(jq -r .[0].commit.committer.date ${tmp}/${rubberneck_github_org}-${rubberneck_github_repo}-commits.json)
 
+curl \
+  -sLo ${tmp}/org-members.json \
+  -H "Accept: application/vnd.github+json" \
+  -H "Authorization: Bearer ${rubberneck_github_token}" \
+  https://api.github.com/orgs/${rubberneck_github_org}/members
+
 ops_username=mobula
 ops_private_key=${HOME}/.ssh/id_manta_ci
 
@@ -56,6 +62,52 @@ for domain in ${domain_list[@]}; do
       https://raw.githubusercontent.com/${rubberneck_github_org}/${rubberneck_github_repo}/${rubberneck_github_latest_sha}/config/${domain}/${hostname}/config.yml; then
       echo "[${fqdn}] config fetch suceeded"
       action=$(yq -r .action ${tmp}/${fqdn}-config.yml)
+
+
+      user_list_as_base64=$(yq -r  '(.user//empty)|.[]|@base64' ${tmp}/${fqdn}-config.yml)
+      for user_as_base64 in ${user_list_as_base64[@]}; do
+        username=$(_decode_property ${user_as_base64} .username)
+        mkdir -p ${tmp}/${fqdn}/${username}
+
+        authorized_key_list_as_base64=$(_decode_property ${user_as_base64} '(.authorized.keys//empty)|.[]|@base64')
+        for authorized_key_as_base64 in ${authorized_key_list_as_base64[@]}; do
+          authorized_key=$(echo ${authorized_key_as_base64} | base64 --decode)
+          echo ${authorized_key} >> ${tmp}/${fqdn}/${username}/authorized_keys_raw
+        done
+        authorized_user_list_as_base64=$(_decode_property ${user_as_base64} '(.authorized.users//empty)|.[]|@base64')
+        for authorized_user_as_base64 in ${authorized_user_list_as_base64[@]}; do
+          authorized_user=$(echo ${authorized_user_as_base64} | base64 --decode)
+          is_org_member=$(jq --arg login ${authorized_user} '. | any((.login | ascii_downcase) == ($login | ascii_downcase))' ${tmp}/org-members.json)
+          echo "[${fqdn}:user:${username}] user: ${authorized_user}, in org: ${is_org_member}"
+          if [ "${authorized_user}" = "manta-ops" ] || [ "${is_org_member}" = "true" ]; then
+            if [ -s ${tmp}/authorized-keys-${authorized_user} ] || curl -sLo ${tmp}/authorized-keys-${authorized_user} https://github.com/${authorized_user}.keys 2> /dev/null; then
+              while read authorized_key; do
+                if [[ ${authorized_key} == ssh-ed25519* ]]; then
+                  echo ${authorized_key} >> ${tmp}/${fqdn}/${username}/authorized_keys_raw
+                fi
+              done < ${tmp}/authorized-keys-${authorized_user}
+            fi
+          fi
+        done
+        if [ "${action}" = "sync" ]; then
+          if [ "${username}" = "root" ]; then
+            remote_path=/root/.ssh/
+          else
+            remote_path=/home/${username}/.ssh/
+          fi
+          sort -u ${tmp}/${fqdn}/${username}/authorized_keys_raw > ${tmp}/${fqdn}/${username}/authorized_keys
+          if [ -s ${tmp}/${fqdn}/${username}/authorized_keys ]; then
+            chmod o-w ${tmp}/${fqdn}/${username}/authorized_keys
+            if rsync -e "ssh -o ConnectTimeout=1 -o StrictHostKeyChecking=accept-new -i ${ops_private_key}" -og --chown=${username}:${username} --rsync-path='sudo rsync' -a ${tmp}/${fqdn}/${username}/authorized_keys ${ops_username}@${fqdn}:${remote_path}; then
+              echo "[${fqdn}:${remote_path}authorized_keys] sync suceeded"
+            else
+              echo "[${fqdn}:${remote_path}authorized_keys] sync failed"
+            fi
+          fi
+        else
+          echo "[${fqdn}:${remote_path}authorized_keys] sync skipped"
+        fi
+      done
 
       command_list_as_base64=$(yq -r '(.command//empty)|.[]|@base64' ${tmp}/${fqdn}-config.yml)
       command_index=0
