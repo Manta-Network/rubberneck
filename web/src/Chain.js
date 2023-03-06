d4ea5ffd58b3fc0b704ff4f9cfd402c62a712baf
Rob Thijssen <rthijssen@gmail.com>
2023-03-06 08:05:53 +0200

    near realtime utilisation metrics

--- web/src/Chain.js
+++ web/src/Chain.js
@@ -2,70 +2,18 @@ import { Link, useParams } from 'react-router-dom';
 import { Fragment, useEffect, useState } from 'react';
 import Col from 'react-bootstrap/Col';
 import Row from 'react-bootstrap/Row';
+import ProgressBar from 'react-bootstrap/ProgressBar';
 import Spinner from 'react-bootstrap/Spinner';
 import Table from 'react-bootstrap/Table';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faAws } from '@fortawesome/free-brands-svg-icons'
-import { faH } from '@fortawesome/free-solid-svg-icons';
 import NodeDiskUsage from './NodeDiskUsage';
 import NodeOs from './NodeOs';
 import NodeConnections from './NodeConnections';
 import NodeHealth from './NodeHealth';
 import RunningCost from './RunningCost';
+import consoleLink from './consoleLink';
 import badge from './badge';
-import { ReactComponent as ShockLogo } from './shock.svg';
-import { ReactComponent as S4yLogo } from './s4y.svg';
-import ProgressBar from 'react-bootstrap/ProgressBar';
 import apiBaseUrl from './apiBaseUrl';
 import getWsConnections from './getWsConnections';
-
-const consoleLink = (node) => {
-  switch (node.provider) {
-    case 'amazon-ec2':
-      return (
-        <a href={node.console.url}>
-          <span style={{marginRight: '0.3em', color: 'rgb(255, 153, 0)', padding: '0 0.2em'}}>
-            <FontAwesomeIcon icon={faAws} />
-          </span>
-          {node.console.text.toLowerCase()}
-        </a>
-      );
-    case 'hetzner-cloud':
-      return (
-        <a href={node.console.url}>
-          <span style={{marginRight: '0.3em', backgroundColor: 'red', color: 'white', padding: '0 0.2em'}}>
-            <FontAwesomeIcon icon={faH} />
-          </span>
-          {node.console.text.toLowerCase()}
-        </a>
-      );
-    case 'hetzner-robot':
-      return (
-        <a href={node.console.url}>
-          <span style={{marginRight: '0.3em', backgroundColor: 'red', color: 'white', padding: '0 0.2em'}}>
-            <FontAwesomeIcon icon={faH} />
-          </span>
-          {node.console.text.toLowerCase()}
-        </a>
-      );
-    case 's4y-dedicated':
-      return (
-        <a href={node.console.url}>
-          <S4yLogo style={{width: '20px', height: '20px', marginRight: '5px'}} />
-          {node.console.text.toLowerCase()}
-        </a>
-      );
-    case 'shock-dedicated':
-      return (
-        <a href={node.console.url}>
-          <ShockLogo style={{width: '20px', height: '20px', marginRight: '5px'}} />
-          {node.console.text.toLowerCase()}
-        </a>
-      );
-    default:
-      return null;
-  }
-};
 
 const threshold = {
   danger: 0.75,
@@ -83,20 +31,23 @@ function Chain(props) {
     : `${apiBaseUrl}/nodes/${relaychain}`;
   useEffect(() => {
     setLoading(true);
-    fetch(apiUrl)
-      .then(response => response.json())
-      .then((container) => {
-        if (!!container.error) {
-          console.error(container.error);
-        } else {
-          setNodes(container.nodes.filter((n) => (n.hostname !== 'ws')).sort((a, b) => (a.fqdn > b.fqdn) ? 1 : (a.fqdn < b.fqdn) ? -1 : 0));
-        }
-        setLoading(false);
-      })
-      .catch((error) => {
-        console.error(error);
-        setLoading(false);
-      });
+    const interval = setInterval(() => {
+      fetch(apiUrl)
+        .then(response => response.json())
+        .then((container) => {
+          if (!!container.error) {
+            console.error(container.error);
+          } else {
+            setNodes(container.nodes.filter((n) => (n.hostname !== 'ws')).sort((a, b) => (a.fqdn > b.fqdn) ? 1 : (a.fqdn < b.fqdn) ? -1 : 0));
+          }
+          setLoading(false);
+        })
+        .catch((error) => {
+          console.error(error);
+          setLoading(false);
+        });
+    }, (3 * 1000));
+    return () => clearInterval(interval);
   }, [relaychain, parachain, apiUrl]);
   useEffect(() => {
     if (!!nodes && !!nodes.length) {
@@ -107,12 +58,19 @@ function Chain(props) {
           [currency]: (!!accumulator[currency]) ? accumulator[currency] + Number(amount) : Number(amount)
         };
       }, {}));
-      (
-        async () => {
-          setWsConnections((await Promise.all(nodes.map((n) => getWsConnections(n.fqdn)))).reduce((a, c) => ({ active: (a.active + c.active), max: (a.max + c.max) }), { active: 0, max: 0 }));
-        }
-      )()
     }
+  }, [nodes]);
+  useEffect(() => {
+    const interval = setInterval(() => {
+      if (!!nodes && !!nodes.length) {
+        (
+          async () => {
+            setWsConnections((await Promise.all(nodes.map((n) => getWsConnections(n.fqdn)))).reduce((a, c) => ({ active: (a.active + c.active), max: (a.max + c.max) }), { active: 0, max: 0 }));
+          }
+        )()
+      }
+    }, (1 * 1000));
+    return () => clearInterval(interval);
   }, [nodes]);
   return (
     <Fragment>
@@ -130,6 +88,26 @@ function Chain(props) {
                     {relaychain.replace('-testnet', 'ᵗ').replace('-internal', 'ᶤ')}
                   </h2>
                 )
+          }
+        </Col>
+        <Col xs={6} style={{textAlign: 'center'}}>
+          {
+            (!!wsConnections.active)
+            ? (
+                <span className="text-muted">
+                  infrastructure utilisation: <strong className="text-dark">
+                    {
+                      new Intl.NumberFormat('default', { style: 'percent', minimumFractionDigits: 1, maximumFractionDigits: 2 }).format((wsConnections.active / wsConnections.max))
+                    }
+                  </strong><br />
+                  active connections: <strong className="text-dark">{Intl.NumberFormat('default').format(wsConnections.active)}</strong>, current capacity: <strong className="text-dark">{Intl.NumberFormat('default').format(wsConnections.max)}</strong>
+                </span>
+              )
+            : (
+                <Spinner style={{...props.style}} animation="grow" size="sm" className="text-secondary">
+                  <span className="visually-hidden">websocket active connection count lookup in progress</span>
+                </Spinner>
+              )
           }
         </Col>
         {
---
+++ web/src/consoleLink.js
@@ -0,0 +1,55 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { ReactComponent as ShockLogo } from './shock.svg';
+import { ReactComponent as S4yLogo } from './s4y.svg';
+import { faAws } from '@fortawesome/free-brands-svg-icons'
+import { faH } from '@fortawesome/free-solid-svg-icons';
+
+const consoleLink = (node) => {
+  switch (node.provider) {
+    case 'amazon-ec2':
+      return (
+        <a href={node.console.url}>
+          <span style={{marginRight: '0.3em', color: 'rgb(255, 153, 0)', padding: '0 0.2em'}}>
+            <FontAwesomeIcon icon={faAws} />
+          </span>
+          {node.console.text.toLowerCase()}
+        </a>
+      );
+    case 'hetzner-cloud':
+      return (
+        <a href={node.console.url}>
+          <span style={{marginRight: '0.3em', backgroundColor: 'red', color: 'white', padding: '0 0.2em'}}>
+            <FontAwesomeIcon icon={faH} />
+          </span>
+          {node.console.text.toLowerCase()}
+        </a>
+      );
+    case 'hetzner-robot':
+      return (
+        <a href={node.console.url}>
+          <span style={{marginRight: '0.3em', backgroundColor: 'red', color: 'white', padding: '0 0.2em'}}>
+            <FontAwesomeIcon icon={faH} />
+          </span>
+          {node.console.text.toLowerCase()}
+        </a>
+      );
+    case 's4y-dedicated':
+      return (
+        <a href={node.console.url}>
+          <S4yLogo style={{width: '20px', height: '20px', marginRight: '5px'}} />
+          {node.console.text.toLowerCase()}
+        </a>
+      );
+    case 'shock-dedicated':
+      return (
+        <a href={node.console.url}>
+          <ShockLogo style={{width: '20px', height: '20px', marginRight: '5px'}} />
+          {node.console.text.toLowerCase()}
+        </a>
+      );
+    default:
+      return null;
+  }
+};
+
+export default consoleLink;
